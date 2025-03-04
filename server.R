e865b815df47fb80bd6b78a9d47200e547dfd061
mf-rug <max.fuerst@gmail.com>
2025-03-04 20:41:39 +0100

    update

--- server.R
+++ server.R
@@ -1,83 +1,160 @@
+color_gradient <- function(dt, column_names, gradient_colors = c("#6666FF", "#DDDDDD", "#FF6666")) {
+  col_func <- colorRampPalette(gradient_colors)
+  
+  for (column_name in column_names) {
+    # Get min & max values of the column
+    col_values <- sort(unique(dt$x$data[[column_name]]), decreasing = FALSE)
+    
+    # Define breakpoints between min & max
+    if (length(col_values) > 1) {
+      breaks <- seq(min(col_values), max(col_values), length.out = length(col_values))
+    } else {
+      breaks <- col_values  # If only one unique value, no breaks needed
+    }
+    
+    dt <- dt %>%
+      formatStyle(
+        column_name, 
+        backgroundColor = styleInterval(
+          breaks[-1],  # Use all breakpoints except the first
+          col_func(length(breaks))  # Generate matching colors
+        )
+      )
+  }
+  
+  return(dt)
+}
+
+
 server <- function(session, input, output) {
-  output$mutation_table <- renderDT({
-    # browser()
-    # req(input$boostmut_csv)
-    # df <- read_csv(input$boostmut_csv$datapath, col_names = TRUE, show_col_types = FALSE) %>% as.data.frame()
-    df <- read_csv('leh_example.csv', col_names = TRUE, show_col_types = FALSE) %>% as.data.frame()
+  data_source <- reactiveVal(NULL)
+  
+  # Observe file input
+  observeEvent(input$boostmut_csv, {
+    data_source("file")
+  })
+  
+  # Observe button click for example data
+  observeEvent(input$example, {
+    data_source("example")
+  })
+  
+  # Reactive dataframe
+  df <- reactive({
+    req(data_source())  # Ensure something has been selected
+    
+    # Determine the source
+    if (data_source() == "file") {
+      req(input$boostmut_csv)
+      path <- input$boostmut_csv$datapath
+    } else {
+      path <- "leh_example2.csv"  # Load example file
+    }
+    
+    print("Loading Data...")
+    df <- read_csv(path, col_names = TRUE, show_col_types = FALSE, name_repair = "unique_quiet") %>% as.data.frame()
+    
     rownames(df) <- df[,1]
     df <- df[,-1]
-    # colnames(df) <- 'E_Hbond'
-    # browser()
-    df$dtm <- as.numeric(df$dtm)
     float_cols <- sapply(df, is.numeric)
-    df$total <- apply(df[,setdiff(float_cols, 'dtm')], 1, mean)
+    df$mean <- apply(df[,float_cols], 1, mean)
     df[, float_cols] <- round(df[, float_cols], 3)
-    df <- df[, c('dtm',
-                 'stabilizing',
-                 'exp_tested',
-                 'total',
-                 setdiff(colnames(df), c(
-                   'dtm', 'stabilizing', 'exp_tested', 'total'
-                 )))]
-    df <- df[order(df$total, decreasing = TRUE),]
     
-    # Define a color function with gradient mapping
-    color_fn <- scales::col_numeric(
-      palette = c("darkblue", "white", "darkred"),  # Gradient colors
-      domain = c(-5, 5)  # Define the range for the mapping
-    )
-    
-    # Apply custom coloring for values outside the range
-    get_color <- function(value) {
-      if (is.na(value)) return('grey')
-      if (value <= -5) return("darkblue")  # Anything ≤ -5 stays dark blue
-      if (value >= 5) return("darkred")    # Anything ≥ 5 stays dark red
-      return(color_fn(value))  # Otherwise, use the gradient
+    df <- df[order(df$mean, decreasing = TRUE), c('mean', setdiff(colnames(df), 'mean'))]
+  })
+
+  
+  output$mutation_table <- renderDT({
+    df <- df()
+    if (input$color_cols == 'All') {
+      target_cols <- colnames(df)
+    } else {
+      target_cols <- 'mean'
     }
-    color_gradient <- function(dt, column_name, gradient_colors = c("#6666FF", "#DDDDDD", "#FF6666")) {
-      col_func <- colorRampPalette(gradient_colors)
-      
-      # Get min & max values of the column
-      col_values <- sort(unique(dt$x$data[[column_name]]), decreasing = FALSE)
-      
-      # Define breakpoints between min & max
-      if (length(col_values) > 1) {
-        breaks <- seq(min(col_values), max(col_values), length.out = length(col_values))
-      } else {
-        breaks <- col_values  # If only one unique value, no breaks needed
-      }
-      
-      dt %>%
-        formatStyle(
-          column_name, 
-          backgroundColor = styleInterval(
-            breaks[-1],  # Use all breakpoints except the first
-            col_func(length(breaks))  # Generate matching colors
-          )
-        ) 
-    }
-    
-    
-    
     datatable(
       df,
       extensions = 'FixedColumns',
       style = 'bootstrap',
       options = list(scrollX = TRUE, # Enable horizontal scrolling
                      fixedColumns = list(leftColumns = 1),  # Freeze the first column
-                     pageLength = 10,
-                     buttons = list('colvis')
-                     
-      )
-    ) %>% 
-      formatStyle(0, target = 'row', lineHeight='75%') %>% 
-      formatStyle('stabilizing', backgroundColor = styleEqual(c(1, 0), c('#b2a919', '#996e80'))) %>% 
-      formatStyle('exp_tested', backgroundColor = styleEqual(c(1, 0), c('#4c4c9d', ''))) %>% 
-      formatStyle(
-        'dtm',
-        backgroundColor = styleEqual(df$dtm, sapply(df$dtm, get_color))  # Apply colors dynamically
-      ) %>% 
-      color_gradient("total")
+                     paging = FALSE,
+                     info = FALSE,
+                     scrollY = '35vh', 
+                     fixedHeader = TRUE
+      )) %>% 
+      formatStyle(0, target = 'row', lineHeight='70%') %>% 
+      color_gradient(target_cols)
     
+  })
+  
+  output$corr_plot_or_not <- renderPlot({
+    req(df(), input$corr_1)
+    df <- df()
+    # browser()
+    ggplot(df) + 
+      geom_point(aes(x = .data[[input$corr_1]], y=.data[[input$corr_2]])) +
+      # geom_point(aes(x = foldx, y=hpsasa_s)) +
+      geom_abline() +
+      coord_fixed() +
+      scale_x_continuous(limits = c(min(df[,c(input$corr_1, input$corr_2)]), max(df[,c(input$corr_1, input$corr_2)]))) +
+      scale_y_continuous(limits = c(min(df[,c(input$corr_1, input$corr_2)]), max(df[,c(input$corr_1, input$corr_2)]))) +
+      theme_bw() +
+      theme(text = element_text(size=18))
+  })
+
+  observe({
+    if (!is.null(df()) && is.data.frame(df()) && nrow(df()) > 0) {
+      shinyjs::show('hidden_metric_choice')
+      shinyjs::show('hidden_metric_choice2')
+      updatePickerInput(session, inputId = 'corr_1', selected = 'mean', choices = colnames(df()))
+      updatePickerInput(session, inputId = 'corr_2', selected = 'foldx', choices = colnames(df()))
+    } else {
+      shinyjs::hide('hidden_metric_choice')
+      shinyjs::hide('hidden_metric_choice2')
+    }
+  })
+
+
+  output$metric_plot_or_not <- renderUI({
+    if (!is.null(input$mutation_table_rows_selected)) { 
+      div(
+        HTML('<span style="text-align:center;"><h5>Metric details</h5></span>'),
+        plotOutput("metric_plot", height='40vh')  # Define the placeholder for the plot
+      )
+    } else if (!is.null(df())) {
+      renderUI({
+        HTML('<i><span style="color: grey;">Select (a) row(s) in the mutation table</span></i>')
+      })
+    }
+  })
+
+  
+  output$metric_plot <- renderPlot({
+    req(input$mutation_table_rows_selected)
+    df <- df()
+    mut_data <- df[input$mutation_table_rows_selected, ] %>% 
+      rownames_to_column(var = 'mutation') %>% 
+      pivot_longer(., -1, names_to = 'metric', values_to = 'score') %>% 
+      as.data.frame()
+    mut_data$metric <- factor(mut_data$metric, levels = rev(colnames(df)))
+    mut_data$color <- 'black'
+    mut_data[mut_data$metric == 'mean', 'color'] <- 'white'
+    # browser()
+    ggplot(mut_data, aes(group = mutation)) +
+      coord_flip() +
+      # geom_vline(xintercept = ncol(df) - 0.5) +
+      geom_rect(xmin = ncol(df) +1, xmax = ncol(df) - 0.5, ymin=-20, ymax=20, color='black', fill = '#0000001c') +
+      geom_col(
+        aes(x = metric, y = score, fill = mutation, color = color),
+        linewidth = 0.5,
+        position = 'dodge'
+      ) +
+      scale_color_identity() +
+      scale_fill_viridis_d(option = "plasma") +
+      scale_y_continuous(limits = (c(min(-2,min(mut_data$score)), c(max(2, max(mut_data$score)))))) +
+      scale_x_discrete(expand = c(0,0)) +
+      ggtitle(paste(unique(mut_data$mutation), collapse = ', ')) +
+      theme_bw() +
+      theme(text = element_text(size=18))
   })
 }
--- ui.R
+++ ui.R
@@ -1,15 +1,60 @@
 library(shiny)
 library(bslib)
 library(readr)
+library(tidyr)
+library(tibble)
+library(ggplot2)
+library(shinyWidgets)
 library(DT)
+library(shinyjs)
 
 ui <- fluidPage(title = 'BoostMut', page_navbar(
   title = 'BoostMut',
+  header = tagList(
+    useShinyjs(),
+    tags$head(
+      tags$style(HTML("
+        /* Target a specific column (e.g., third column, index starts from 1) */
+        table.dataTable tbody tr.even td:nth-child(2) {
+          box-shadow: inset 0 0 0 9999px rgba(0, 0, 0, 0) !important;
+        }
+            /* Default button styling */
+      .btn-default {
+          font-size: 11pt;
+          padding-right: 11px;
+          padding-left: 11px;
+      }
+      "))
+  )),
   nav_spacer(),
   # push nav items to the right
   nav_item(input_dark_mode(id = "dark_mode")),
-  sidebarLayout(sidebarPanel(
-    fileInput('boostmut_csv', 'Select BoostMut csv output', accept = 'csv'),
-    width = 3
-  ), mainPanel(DTOutput('mutation_table', width = '100%'), width = 9))
+
+  nav_panel(NULL, 
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('boostmut_csv', 'Select BoostMut .csv output', accept = 'csv'),
+      actionButton('example', 'Load example'),
+      br(),br(),
+      radioGroupButtons('color_cols',
+                        'Columns to highlight',
+                        choices = c('All', 'Only mean')),
+      width = 2
+  ), 
+  mainPanel(
+    DTOutput('mutation_table', width = '100%'), 
+    shinyjs::hidden(div(id = 'hidden_metric_choice',hr())),
+    fluidRow(
+      column(5, 
+             shinyjs::hidden(div(id = 'hidden_metric_choice2',
+                                 HTML('<span style="text-align:center;"><h5>Correlation analysis</h5></span>'),
+                                 fluidRow(
+                                   column(6, pickerInput('corr_1', 'Metric 1', width = 'fit', choices = NULL), align = 'right'),
+                                   column(6, pickerInput('corr_2', 'Metric 2', width = 'fit', choices = NULL), align = 'left')
+             ))),
+             plotOutput('corr_plot_or_not')),
+      column(7, uiOutput('metric_plot_or_not'))
+    ),
+    width = 10))
+  )
 ))
